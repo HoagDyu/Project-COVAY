ee73ac047c0bb5fb4e97ffaff9a2199affef90a9
Huỳnh Trần Đức Duy <kenhuynh60@gmail.com>
2026-07-05 05:27:51 +0700

    Chinh lai import

--- ai/class_bot.py
+++ ai/class_bot.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from bot_algorithm import choose_best_move
+from .bot_algorithm import choose_best_move
 from core.constant import StoneColor
 from model.entities import Player
 
