fdecb8b7dff6780d599379970159162952df99c3
Huỳnh Trần Đức Duy <kenhuynh60@gmail.com>
2026-07-04 19:07:44 +0700

    modify bot

--- ai/class_bot.py
+++ ai/class_bot.py
@@ -2,48 +2,11 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from .another_bot_algorithm import choose_best_move
+from bot_algorithm import choose_best_move
 from core.constant import StoneColor
 from model.entities import Player
 
-try:
-    from PyQt6.QtCore import QObject, pyqtSignal
-except ModuleNotFoundError:
-    class _FallbackSignal:
-        def __init__(self):
-            self._slots = []
-
-        def connect(self, slot):
-            self._slots.append(slot)
-
-        def emit(self, *args, **kwargs):
-            for slot in list(self._slots):
-                slot(*args, **kwargs)
-
-    class _FallbackSignalDescriptor:
-        def __set_name__(self, owner, name):
-            self.name = f"_{name}"
-
-        def __get__(self, instance, owner):
-            if instance is None:
-                return self
-            if not hasattr(instance, self.name):
-                setattr(instance, self.name, _FallbackSignal())
-            return getattr(instance, self.name)
-
-    def pyqtSignal(*_args, **_kwargs):
-        return _FallbackSignalDescriptor()
-
-    class QObject:
-        def __init__(self, parent=None):
-            self.parent = parent
-
-        def moveToThread(self, thread):
-            self.thread = thread
-
-        def deleteLater(self):
-            pass
-
+from PyQt6.QtCore import QObject, pyqtSignal
 
 if TYPE_CHECKING:
     from model.board_logic import BoardLogic
